bca429b275217fff828435236e0f3655048be9e6
Demeter Dzadik <demeterdzadik@gmail.com>
2020-07-03 18:55:20 +0200

    finish eye group master widget generation logic

--- widgets.py
+++ widgets.py
@@ -2,47 +2,43 @@
 # TODO: but also, move code for load_widgets() in here, cause why not.
 
 from mathutils import Matrix, Vector
-from math import pi, sin, cos
+from math import pi, sin, cos, acos
 import bpy
 
 def bezier_widget(rig, coords, bone):
-    """Create a bezier curve widget where coords is a list of Vectors that the curve should be near.
-    Find the outer-most points, ignore any "inner" ones.
-    Prune remaining points that are too close to each other. (TODO: what does too close mean, it would mean different things at different scales)
-    Flatten remaining points so that they fit on a flat plane.
-    Sort these points starting with an arbitrary one, the next one is the nearest one, the next one is the nearest one that wasn't already sorted.
-    Place bezier curve points at the coordinates.
-    Convert the curve to mesh and return the object.
-    """
+    """Create a bezier curve widget where coords is a list of Vectors that the curve should be near."""
 
-    ob_name = "curveobname"
-    data_name = "curvename"
+    ob_name = "WGT-" + bone.name
+    data_name = "WGT-" + bone.name
 
     bpy.ops.object.mode_set(mode='OBJECT')
 
-    # Delete if exists (TODO: put under Force Widget Update check)
-    # obj = bpy.data.objects.get(ob_name)
-    # if obj:
-    #     bpy.data.objects.remove(obj)
-    #     bpy.data.curves.remove(obj.data)
+    # If the object exists, delete it.
+    obj = bpy.data.objects.get(ob_name)
+    if obj:
+        obdata = obj.data
+        bpy.data.objects.remove(obj)
 
     curve = bpy.data.curves.new(data_name, 'CURVE')
     curve.dimensions = '3D'
+    obj = bpy.data.objects.new(ob_name, curve)
+
     spline = curve.splines.new('BEZIER')
     spline.use_cyclic_u = True
-    obj = bpy.data.objects.new(ob_name, curve)
+
     bpy.context.scene.collection.objects.link(obj)
 
     # Do all the fancy maths by Ivan Cappiello & MAD studios - TODO: ask for permission to use this code, or re-write it a bit nicer.
     coords = project_points_on_plane(coords, bone.vector)
-    # coords = get_2d_border(coords, double=False)[0]
     
     # Create and place the spline points
     spline.bezier_points.add(len(coords)-1)
-    print("coords:\n\n")
     for i, p in enumerate(spline.bezier_points):
-        p.co = coords[i][:3]
-        print(p.co)
+        co = coords[i]
+        # world->bone axis shuffle...
+        p.co[0] = co[0]
+        p.co[1] = co[2]
+        p.co[2] = -co[1]
         p.handle_left_type = 'AUTO'
         p.handle_right_type = 'AUTO'
     
@@ -51,31 +47,52 @@ def bezier_widget(rig, coords, bone):
     bpy.context.view_layer.objects.active = obj
     obj.select_set(True)
     bpy.ops.object.convert(target='MESH')
+
+    # Assign to widget collection
     obj = bpy.context.object
+    obj.data.name = obj.name
     rig.generator.wgt_collection.objects.link(obj)
     bpy.context.scene.collection.objects.unlink(obj)
 
+    # Restore selection and modes
     bpy.context.view_layer.objects.active = rig.obj
     rig.obj.select_set(True)
     bpy.ops.object.mode_set(mode='EDIT')
 
     return obj
 
+def sort_circular_points(points, axis):
+    """Sort a list of points based on their angle around an axis."""
+    anlges = [(point, angle(point, axis)) for point in points]
+
+    # sort by angle...
+    
+    bpy.context.scene['linked_points'] = [vec[:] for vec in linked_points]
+    
+    return linked_points
+
 def project_points_on_plane (points, projection_axis):
-    # Define a plane based on the projection axis: find two vectors(ie. a plane) that are perpendicular to the projection axis.
-    plane_x = projection_axis.cross(Vector((0.142523434, 0.1123124, 1)))
-    plane_y = projection_axis.cross(plane_x)
+    # Find two vectors(ie. a plane) that are perpendicular to the projection axis.
+    projection_direction = projection_axis.normalized()
+    plane_x = projection_direction.cross(Vector((0, 0, 1)))
+    plane_y = projection_direction.cross(plane_x)
 
     projected_points = []
     points_sum = Vector()
     for point in points:
         points_sum += point
-    
+
     points_center = points_sum / len(points) # TODO: use bounding box instead of average
 
     for point in points:
         center_relative = point - points_center
         projected_point = Vector((center_relative.dot(plane_x), center_relative.dot(plane_y), 0))
-        projected_points.append(projected_point)
+        
+        angle_from_axis = acos(projected_point.dot(plane_x) / (projected_point.length * plane_x.length))
 
-    return projected_points
+        projected_points.append((projected_point, angle_from_axis))
+
+    # Sort points by their angle from the projection axis
+    projected_points.sort(key=lambda x: x[1])
+
+    return [p[0] for p in projected_points]
