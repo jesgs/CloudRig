49971d285642f9fd2b97947ea824152b31c3b866
Demeter Dzadik <demeterdzadik@gmail.com>
2020-07-07 18:09:27 +0200

    add copy_type bool to Copy Rigify Parameters operator

--- operators/mirror_rigify.py
+++ operators/mirror_rigify.py
@@ -1,8 +1,8 @@
 import bpy
 from ..rigs import cloud_utils
+from bpy.props import BoolProperty
 
 def copy_rigify_params(from_bone, to_bone):
-	to_bone.rigify_type = from_bone.rigify_type
 	for key, value in from_bone.rigify_parameters.items():
 		try:
 			setattr(to_bone.rigify_parameters, key, getattr(from_bone.rigify_parameters, key))
@@ -33,6 +33,7 @@ class MirrorRigifyParameters(bpy.types.Operator):
 				print(f"Warning: Bone {pb.name} selected on both sides, mirroring would be ambiguous, skipping. Only select the left or right side, not both!")
 				continue
 			
+			flip_bone.rigify_type = pb.rigify_type
 			copy_rigify_params(pb, flip_bone)
 
 		return { 'FINISHED' }
@@ -44,10 +45,12 @@ class CopyRigifyParameters(bpy.types.Operator):
 	bl_label = "Copy Rigify Parameters To Selected Bones"
 	bl_options = {'REGISTER', 'UNDO'}
 
+	copy_type: BoolProperty(name="Copy Rigify Type", default=True)
+
 	@classmethod
 	def poll(cls, context):
 		obj = context.object
-		return obj and obj.type=='ARMATURE' and obj.mode=='POSE' and len(context.selected_pose_bones)>1 and context.active_pose_bone!=None and context.active_pose_bone.rigify_type!=""
+		return obj and obj.type=='ARMATURE' and obj.mode=='POSE' and len(context.selected_pose_bones)>1 and context.active_pose_bone!=None
 
 	def execute(self, context):
 		rig = context.object
@@ -55,6 +58,8 @@ class CopyRigifyParameters(bpy.types.Operator):
 
 		for pb in context.selected_pose_bones:
 			if pb == active_bone: continue
+			if self.copy_type:
+				pb.rigify_type = active_bone.rigify_type
 			copy_rigify_params(active_bone, pb)
 
 		return { 'FINISHED' }
