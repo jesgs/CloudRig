16a601d06996b86bc3bdfee3c24dadec9853457c
Demeter Dzadik <demeterdzadik@gmail.com>
2020-07-14 20:12:01 +0200

    Actions: UI and utility for symmetry option

--- actions.py
+++ actions.py
@@ -1,6 +1,8 @@
 import bpy
 from bpy.props import EnumProperty, IntProperty, BoolProperty, StringProperty, FloatProperty, PointerProperty
 
+from .utils import flip_name
+
 class CLOUDRIG_OT_Action_Remove(bpy.types.Operator):
 	"""Remove an action setup"""
 
@@ -118,6 +120,11 @@ class CloudRigAction(bpy.types.PropertyGroup):
 		,description = "Create constraints for this action on the generated rig"
 		,default=True
 	)
+	symmetrical: BoolProperty(
+		name="Symmetrical"
+		,description = "Apply the same setup but mirrored to the opposite side control, shown in parentheses. Bones will only be affected by the control with the same side (eg., .L bones will only be affected by the .L control). Bones without a side in their name (so no .L or .R) will be affected by both controls with 0.5 influence each"
+		,default=True
+	)
 	action: PointerProperty(name="Action", type=bpy.types.Action)
 	subtarget: StringProperty(name="Control Bone", description="Select a bone on the generated rig which will drive this action")
 
@@ -200,8 +207,14 @@ def draw_cloudrig_actions(layout, rig):
 	row = layout.row()
 	subtarget_exists = act.subtarget in rig.data.rigify_target_rig.data.bones
 	icon = 'BONE_DATA' if subtarget_exists else 'ERROR'
+	flipped_subtarget = flip_name(act.subtarget)
 	row.prop_search(act, 'subtarget', rig.data.rigify_target_rig.data, 'bones', icon=icon)
 	row.alert = not subtarget_exists
+
+	if subtarget_exists and (act.subtarget.endswith(".L") or act.subtarget.endswith(".R")):
+		row = layout.row()
+		row.use_property_split=True
+		row.prop(act, 'symmetrical', text=f"Symmetrical ({flipped_subtarget})")
 
 	if not subtarget_exists: return
 	layout.prop(act, 'frame_start', text="Frame Start")
