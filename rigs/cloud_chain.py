dd5f9285d5d00a92875ccb45881dbd7c7db50d6b
Demeter Dzadik <demeterdzadik@gmail.com>
2020-07-21 12:55:44 +0200

    some more type hinting, comment cleanup, and nuke create_sample() for now since it's so outdated.

--- rigs/cloud_chain.py
+++ rigs/cloud_chain.py
@@ -1,16 +1,19 @@
 from typing import Tuple, List
+from ..definitions.bone import BoneInfo, BoneSet
 
 from bpy.props import BoolProperty, IntProperty
 
-from ..definitions.bone import BoneInfo, BoneSet
 from .cloud_base import CloudBaseRig
 
 """
-TODO: Allow for circular chains, if the first bone's head is the same location as the last bone's tail, and final control is enabled. Doesn't have to be a parameter.
+TODO: Allow for circular chains, if the first bone's head is the same location
+as the last bone's tail, and final control is enabled. Doesn't have to be a parameter.
 
 Ideas:
 Spline IK like controls(the other two types) for bendy bones' handles.
-Recursive generation of STR layers as per Pablo's request, so we don't just have main and sub STR controls, but any number of nested layers(although we would probably never use more than 3, but then again, I thought we would never use more than 2, so)
+Recursive generation of STR layers as per Pablo's request, so we don't just have
+main and sub STR controls, but any number of nested layers(although we would
+probably never use more than 3, but then again, I thought we would never use more than 2, so)
 """
 
 class CloudChainRig(CloudBaseRig):
@@ -23,7 +26,10 @@ class CloudChainRig(CloudBaseRig):
 		self.chain_length = 0
 
 	def ensure_bone_sets(self):
-		# TODO: We should introduce a convention that bone sets ending in _chain are ones where the order is expected to be meaningful. If our STR bones could be ordered, our code could be a bit cleaner. And they are ordered, it's just not clear.
+		# TODO: We should introduce a convention that bone sets ending in 
+		# _chain are ones where the order is expected to be meaningful. 
+		# If our STR bones could be ordered, our code could be a bit cleaner. 
+		# And they are ordered, it's just not clear.
 		super().ensure_bone_sets()
 		self.str_bones = self.ensure_bone_set("Stretch Controls")
 		self.str_mch = self.ensure_bone_set("Stretch Helpers")
@@ -48,11 +54,12 @@ class CloudChainRig(CloudBaseRig):
 
 		self.connect_parent_chain_rig()
 
-	def determine_segments(self, org_bone) -> Tuple[int, int]:
+	def determine_segments(self, org_bone: BoneInfo) -> Tuple[int, int]:
 		"""Determine how many deform and b-bone segments should be in a section of the chain."""
 		segments = self.params.CR_deform_segments
 
-		bbone_density = round(org_bone.length/self.average_org_length * self.params.CR_bbone_density * self.params.CR_deform_segments)
+		bbone_density = round(org_bone.length/self.average_org_length * 
+			self.params.CR_bbone_density * self.params.CR_deform_segments)
 
 		# No segments for last bone of the chain if there is no control for its tail.
 		if org_bone == self.org_chain[-1] and not self.params.CR_cap_control:
@@ -60,7 +67,8 @@ class CloudChainRig(CloudBaseRig):
 		
 		return segments, bbone_density
 
-	def make_str_chain(self, org_chain) -> List[List[BoneInfo]]:
+	def make_str_chain(self, org_chain: BoneSet) -> List[List[BoneInfo]]:
+		"""Create all STR controls for this chain."""
 		self.main_str_bones: List[BoneInfo] = []
 		str_sections = []
 		for org_i, org_bone in enumerate(org_chain):
@@ -90,7 +98,8 @@ class CloudChainRig(CloudBaseRig):
 		
 		return str_sections
 
-	def make_str_bone(self, org_bone, seg_i, segments):
+	def make_str_bone(self, org_bone: BoneInfo, seg_i: int, segments: int) -> BoneInfo:
+		"""Create an STR control."""
 		direction = org_bone.vector
 		if seg_i==0 and org_bone.prev:
 			direction = org_bone.tail - org_bone.prev.head
@@ -114,7 +123,7 @@ class CloudChainRig(CloudBaseRig):
 		return str_bone
 
 	def make_str_helpers(self, str_sections: List[List[BoneInfo]]):
-		"""STR-H are mechanism bones that keep STR controls between two main STR controls."""
+		"""Create STR-H bones that keep STR controls between two main STR controls."""
 		main_str_bone = None
 		for sec_i, section in enumerate(str_sections):
 			for i, str_bone in enumerate(section):
@@ -140,24 +149,44 @@ class CloudChainRig(CloudBaseRig):
 				last_str = str_sections[sec_i+1][0].name
 				influence_unit = 1 / len(section)
 				influence = i * influence_unit
-				str_h_bone.add_constraint('COPY_LOCATION', space='WORLD', subtarget=first_str)
-				str_h_bone.add_constraint('COPY_LOCATION', space='WORLD', subtarget=last_str, influence=influence)
+				str_h_bone.add_constraint('COPY_LOCATION'
+					,space		= 'WORLD'
+					,subtarget	= first_str
+				)
+				str_h_bone.add_constraint('COPY_LOCATION'
+					,space		= 'WORLD'
+					,subtarget	= last_str
+					,influence	= influence)
 
-				str_h_bone.add_constraint('COPY_ROTATION', space='WORLD', subtarget=first_str)
-				str_h_bone.add_constraint('COPY_ROTATION', space='WORLD', subtarget=last_str, influence=influence)
+				str_h_bone.add_constraint('COPY_ROTATION'
+					,space		= 'WORLD'
+					,subtarget	= first_str
+				)
+				str_h_bone.add_constraint('COPY_ROTATION'
+					,space		= 'WORLD'
+					,subtarget	= last_str
+					,influence	= influence
+				)
 				str_h_bone.add_constraint('DAMPED_TRACK', subtarget=last_str)
 
-	def make_dt_helper(self, str_bone) -> BoneInfo:
-		"""Create a child bone for an STR bone with Damped Track constraints to aim at the previous and next STR bones."""
+	def make_dt_helper(self, str_bone: BoneInfo) -> BoneInfo:
+		"""Create a child bone for an STR bone with Damped Track constraints 
+		to aim at the previous and next STR bones."""
 		dt_bone = self.str_mch.new(
 			name = self.add_prefix_to_name(str_bone, "DT")
 			,source = str_bone
 			,parent = str_bone
 		)
 		if str_bone.next:
-			pos_con = dt_bone.add_constraint('DAMPED_TRACK', subtarget = str_bone.next.name, track_axis='TRACK_Y')
+			pos_con = dt_bone.add_constraint('DAMPED_TRACK'
+				,subtarget = str_bone.next.name
+				,track_axis='TRACK_Y'
+			)
 		if str_bone.prev:
-			neg_con = dt_bone.add_constraint('DAMPED_TRACK', subtarget = str_bone.prev.name, track_axis='TRACK_NEGATIVE_Y')
+			neg_con = dt_bone.add_constraint('DAMPED_TRACK'
+				,subtarget = str_bone.prev.name
+				,track_axis='TRACK_NEGATIVE_Y'
+			)
 			if str_bone.next:
 				neg_con.influence = 0.5
 		dt_bone.add_constraint('COPY_ROTATION', subtarget = str_bone.name, mix_mode='BEFORE')
@@ -166,7 +195,7 @@ class CloudChainRig(CloudBaseRig):
 
 		return dt_bone
 
-	def make_def_chain(self, str_chain) -> BoneSet:
+	def make_def_chain(self, str_chain: BoneSet) -> BoneSet:
 		"""Create a deform chain stretching from one STR bone to the next"""
 		for str_i, str_bone in enumerate(str_chain):
 			# Skip the tip control
@@ -229,13 +258,17 @@ class CloudChainRig(CloudBaseRig):
 
 		return self.def_bones
 
-	def make_shape_key_helper(self, def_bone_1, def_bone_2) -> BoneInfo:
-		"""The goal is to accurately read the rotational difference between def_bone_1 and def_bone_2, each of which can be a bendy bone.
-		SKP (Shape Key Helper Parent): Copy Transforms of the b-bone tail of of def_bone_1.
-		SKH (Shape Key Helper): This is parented to SKP and Copy Transforms of the b-bone head of def_bone_2.
-		Reading the local rotation of SKH should now give us the rotation which we can use to activate corrective shape keys.
+	def make_shape_key_helper(self, def_bone_1: BoneInfo, def_bone_2: BoneInfo) -> BoneInfo:
+		"""Create SKP and SKH helper bones.
+		
+		Reading the local rotation of SKH
+		should give us the rotation which we can use to activate corrective
+		shape keys, since it will always represent the true rotational
+		difference between the end of def_bone_1 and the start of def_bone_2.
 		"""
 
+		# SKP (Shape Key Helper Parent): Copy Transforms of the b-bone tail 
+		# of def_bone_1.
 		skp_bone = self.skh_bones.new(
 			name		 = def_bone_1.name.replace("DEF", "SKP")
 			,source		 = def_bone_1
@@ -252,6 +285,8 @@ class CloudChainRig(CloudBaseRig):
 			,head_tail		 = 1
 		)
 
+		# SKH (Shape Key Helper): This is parented to SKP and Copy Transforms 
+		# of the b-bone head of def_bone_2.
 		skh_bone = self.skh_bones.new(
 			name		 = def_bone_1.name.replace("DEF", "SKH")
 			,source		 = def_bone_1
@@ -271,7 +306,12 @@ class CloudChainRig(CloudBaseRig):
 		return skh_bone
 
 	def connect_parent_chain_rig(self):
-		# If the parent rig is a connected chain rig with cap_control=False, make the last DEF bone of that rig stretch to this rig's first STR.
+		"""Connect two separate but connected cloud_chain rigs.
+		
+		If the parent rig is a connected chain rig with cap_control=False, 
+		make the last DEF bone of that rig stretch to this rig's first STR.
+		"""
+
 		parent_rig = self.rigify_parent
 		if isinstance(parent_rig, CloudChainRig):
 			if not parent_rig.params.CR_cap_control:
@@ -291,7 +331,7 @@ class CloudChainRig(CloudBaseRig):
 
 	@classmethod
 	def define_bone_sets(cls, params):
-		""" Create parameters for this rig's bone sets. """
+		"""Create parameters for this rig's bone sets."""
 		super().define_bone_sets(params)
 		cls.define_bone_set(params, "Stretch Controls", preset=8,	default_layers=[cls.default_layers('STRETCH')])
 		cls.define_bone_set(params, "Stretch Helpers",				default_layers=[cls.default_layers('MCH')], override='MCH')
@@ -301,7 +341,7 @@ class CloudChainRig(CloudBaseRig):
 	@classmethod
 	def add_parameters(cls, params):
 		""" Add the parameters of this rig type to the
-			RigifyParameters PropertyGroup
+			RigifyParameters PropertyGroup.
 		"""
 		super().add_parameters(params)
 
@@ -347,8 +387,7 @@ class CloudChainRig(CloudBaseRig):
 
 	@classmethod
 	def cloud_params_ui(cls, layout, params):
-		""" Create the ui for the rig parameters.
-		"""
+		"""Create the ui for the rig parameters."""
 		layout = super().cloud_params_ui(layout, params)
 
 		if not cls.cloud_dropdown_ui(layout, params, "CR_show_chain_settings"): return layout
@@ -367,148 +406,3 @@ class CloudChainRig(CloudBaseRig):
 
 class Rig(CloudChainRig):
 	pass
-
-import bpy
-
-def create_sample(obj):
-    # generated by rigify.utils.write_metarig
-    bpy.ops.object.mode_set(mode='EDIT')
-    arm = obj.data
-
-    bones = {}
-
-    bone = arm.edit_bones.new('Chain_1')
-    bone.head = 0.0000, 0.0000, 0.0000
-    bone.tail = 0.0000, -0.5649, 0.0000
-    bone.roll = -3.1416
-    bone.use_connect = False
-    bone.bbone_x = 0.0399
-    bone.bbone_z = 0.0399
-    bone.head_radius = 0.0565
-    bone.tail_radius = 0.0282
-    bone.envelope_distance = 0.1412
-    bone.envelope_weight = 1.0000
-    bone.use_envelope_multiply = 0.0000
-    bones['Chain_1'] = bone.name
-    bone = arm.edit_bones.new('Chain_2')
-    bone.head = 0.0000, -0.5649, 0.0000
-    bone.tail = 0.0000, -1.1299, 0.0000
-    bone.roll = -3.1416
-    bone.use_connect = True
-    bone.bbone_x = 0.0399
-    bone.bbone_z = 0.0399
-    bone.head_radius = 0.0282
-    bone.tail_radius = 0.0565
-    bone.envelope_distance = 0.1412
-    bone.envelope_weight = 1.0000
-    bone.use_envelope_multiply = 0.0000
-    bone.parent = arm.edit_bones[bones['Chain_1']]
-    bones['Chain_2'] = bone.name
-    bone = arm.edit_bones.new('Chain_3')
-    bone.head = 0.0000, -1.1299, 0.0000
-    bone.tail = 0.0000, -1.6948, -0.0000
-    bone.roll = -3.1416
-    bone.use_connect = True
-    bone.bbone_x = 0.0399
-    bone.bbone_z = 0.0399
-    bone.head_radius = 0.0565
-    bone.tail_radius = 0.0565
-    bone.envelope_distance = 0.1412
-    bone.envelope_weight = 1.0000
-    bone.use_envelope_multiply = 0.0000
-    bone.parent = arm.edit_bones[bones['Chain_2']]
-    bones['Chain_3'] = bone.name
-    bone = arm.edit_bones.new('Chain_4')
-    bone.head = 0.0000, -1.6948, -0.0000
-    bone.tail = 0.0000, -2.2598, 0.0000
-    bone.roll = -3.1416
-    bone.use_connect = True
-    bone.bbone_x = 0.0399
-    bone.bbone_z = 0.0399
-    bone.head_radius = 0.0565
-    bone.tail_radius = 0.0565
-    bone.envelope_distance = 0.1412
-    bone.envelope_weight = 1.0000
-    bone.use_envelope_multiply = 0.0000
-    bone.parent = arm.edit_bones[bones['Chain_3']]
-    bones['Chain_4'] = bone.name
-
-    bpy.ops.object.mode_set(mode='OBJECT')
-    pbone = obj.pose.bones[bones['Chain_1']]
-    pbone.rigify_type = 'cloud_chain'
-    pbone.lock_location = (False, False, False)
-    pbone.lock_rotation = (False, False, False)
-    pbone.lock_rotation_w = False
-    pbone.lock_scale = (False, False, False)
-    pbone.rotation_mode = 'QUATERNION'
-    try:
-        pbone.rigify_parameters.CR_subdivide_deform = 10
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_controls_for_handles = True
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_show_spline_ik_settings = True
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_show_display_settings = False
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_rotatable_handles = False
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_hook_name = "Cable"
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_show_chain_settings = True
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_sharp_sections = False
-    except AttributeError:
-        pass
-    try:
-        pbone.rigify_parameters.CR_bbone_density = 6
-    except AttributeError:
-        pass
-    pbone = obj.pose.bones[bones['Chain_2']]
-    pbone.rigify_type = ''
-    pbone.lock_location = (False, False, False)
-    pbone.lock_rotation = (False, False, False)
-    pbone.lock_rotation_w = False
-    pbone.lock_scale = (False, False, False)
-    pbone.rotation_mode = 'QUATERNION'
-    pbone = obj.pose.bones[bones['Chain_3']]
-    pbone.rigify_type = ''
-    pbone.lock_location = (False, False, False)
-    pbone.lock_rotation = (False, False, False)
-    pbone.lock_rotation_w = False
-    pbone.lock_scale = (False, False, False)
-    pbone.rotation_mode = 'QUATERNION'
-    pbone = obj.pose.bones[bones['Chain_4']]
-    pbone.rigify_type = ''
-    pbone.lock_location = (False, False, False)
-    pbone.lock_rotation = (False, False, False)
-    pbone.lock_rotation_w = False
-    pbone.lock_scale = (False, False, False)
-    pbone.rotation_mode = 'QUATERNION'
-
-    bpy.ops.object.mode_set(mode='EDIT')
-    for bone in arm.edit_bones:
-        bone.select = False
-        bone.select_head = False
-        bone.select_tail = False
-    for b in bones:
-        bone = arm.edit_bones[bones[b]]
-        bone.select = True
-        bone.select_head = True
-        bone.select_tail = True
-        arm.edit_bones.active = bone
-
-    return bones
