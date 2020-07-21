67586dd4dfa79152d9de1b680b16a4c1ceac2405
Demeter Dzadik <demeterdzadik@gmail.com>
2020-07-21 14:26:54 +0200

    naming: cleanup

--- utils/naming.py
+++ utils/naming.py
@@ -10,10 +10,23 @@ def get_name(thing) -> str:
 	else:
 		return str(thing)
 
-class CloudNamingUtilitiesMixin:
-	"""Name management utilities with the convenience of being able to pass in anything that has a "name" attribute, or strings directly."""
+"""Idea: Instead of implementing this class as a Mixin class, it should instead
+stand on its own, and simply have an instance on cloud_base and cloud_generator.
 
-	def __init__(self, prefix_separator="_", suffix_separator=".", side_suffix="L", side_prefix="Left", **kwargs):
+This has the downside of making some calls longer, eg. "self.naming.make_name()"
+instead of "self.make_name" but the upside of not having to use 
+multiple-inheritance, which is confusing and hard to read.
+
+If we do this, this could be renamed to class NameManager.
+"""
+
+class CloudNamingUtilitiesMixin:
+	"""Name management utilities with the convenience of being able to pass in 
+	anything that has a "name" attribute, or strings directly.
+	"""
+
+	def __init__(self, prefix_separator="_", suffix_separator=".", 
+				side_suffix="L", side_prefix="Left", **kwargs):
 		self.prefix_separator = prefix_separator
 		self.suffix_separator = suffix_separator
 		self.side_suffix = side_suffix
@@ -33,7 +46,7 @@ class CloudNamingUtilitiesMixin:
 
 		return slice_name(name, prefix_separator, suffix_separator)
 
-	def strip_trailing_numbers(self, thing) -> str:
+	def strip_trailing_numbers(self, thing) -> Tuple[str, str]:
 		return strip_trailing_numbers(get_name(thing))
 
 	def flipped_name(self, thing) -> str:
@@ -61,8 +74,9 @@ class CloudNamingUtilitiesMixin:
 		sliced_name[0].append(new_prefix)
 		return self.make_name(*sliced_name)
 	
-def make_name(prefixes=[], base="", suffixes=[], prefix_separator="-", suffix_separator="."):
-	# In our naming convention, prefixes are separated by dashes and suffixes by periods, eg: DSP-FK-UpperArm_Parent.L.001
+def make_name(prefixes=[], base="", suffixes=[], 
+			  prefix_separator="-", suffix_separator=".") -> str:
+	"""Make a name from a list of prefixes, a base, and a list of suffixes."""
 	name = ""
 	for pre in prefixes:
 		if pre=="": continue
@@ -74,12 +88,13 @@ def make_name(prefixes=[], base="", suffixes=[], prefix_separator="-", suffix_se
 	return name
 
 def slice_name(name, prefix_separator="-", suffix_separator="."):
+	"""Break up a name into its prefix, base, suffix components."""
 	prefixes = name.split(prefix_separator)[:-1]
 	suffixes = name.split(suffix_separator)[1:]
 	base = name.split(prefix_separator)[-1].split(suffix_separator)[0]
 	return [prefixes, base, suffixes]
 
-def strip_trailing_numbers(name):
+def strip_trailing_numbers(name) -> Tuple[str, str]:
 	if "." in name:
 		# Check if there are only digits after the last period
 		slices = name.split(".")
@@ -92,10 +107,16 @@ def strip_trailing_numbers(name):
 
 	return name, ""
 
-def flip_name(from_name, ignore_base=True, must_change=False):
-	# based on BLI_string_flip_side_name in https://developer.blender.org/diffusion/B/browse/master/source/blender/blenlib/intern/string_utils.c
-	# If ignore_base==True, ignore occurrences of side hints unless they're in the beginning or end of the name string.
-	# if must_change==True, raise an error if the string couldn't be flipped.
+def flip_name(from_name, ignore_base=True, must_change=False) -> str:
+	"""Turn a left-sided name into a right-sided one or vice versa.
+
+	Based on BLI_string_flip_side_name:
+	https://developer.blender.org/diffusion/B/browse/master/source/blender/blenlib/intern/string_utils.c
+
+	ignore_base: When True, ignore occurrences of side hints unless they're in 
+				 the beginning or end of the name string.
+	must_change: When True, raise an error if the name couldn't be flipped.
+	"""
 
 	# Handling .### cases
 	stripped_name, number_suffix = strip_trailing_numbers(from_name)
@@ -127,10 +148,10 @@ def flip_name(from_name, ignore_base=True, must_change=False):
 
 	if(must_change):
 		assert new_name != from_name, "Failed to flip string: " + from_name
-	
+
 	return new_name
 
-def combine_bone_names(names, side_suf=".L", side_pref="L_"):
+def combine_bone_names(names, side_suf=".L", side_pref="L_") -> str:
 	"""Combine multiple bone names into one."""
 	# This is the most terrible code I have ever written.
 
@@ -164,7 +185,7 @@ def combine_bone_names(names, side_suf=".L", side_pref="L_"):
 
 	### Combine suffixes
 	suffixes_nonunique = [slice_name(n)[2] for n in names]
-	suffixes = []
+	suffixes: List[str] = []
 	for suf_list in suffixes_nonunique:
 		for suf in suf_list:
 			if suf not in suffixes:
@@ -176,7 +197,7 @@ def combine_bone_names(names, side_suf=".L", side_pref="L_"):
 
 	### Combine prefixes
 	prefixes_nonunique = [slice_name(n)[0] for n in names]
-	prefixes = []
+	prefixes: List[str] = []
 	for pre_list in prefixes_nonunique:
 		for pre in pre_list:
 			if pre not in prefixes:
@@ -189,11 +210,11 @@ def combine_bone_names(names, side_suf=".L", side_pref="L_"):
 	### Combine and return the result
 	return make_name(prefixes, final_base, suffixes)
 
-def name_side_is_left(name):
+def name_side_is_left(name) -> Optional[bool]:
 	"""Identify whether a name belongs to the left or right side or neither."""
 
 	flipped_name = flip_name(name)
-	if flipped_name==name: return	# Return None to indicate neither side.
+	if flipped_name==name: return None	# Return None to indicate neither side.
 
 	stripped_name, number_suffix = strip_trailing_numbers(name)
 
