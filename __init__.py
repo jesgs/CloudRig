8dbd7cb580fba7ee7972826f905586b66fd17a0c
Demeter Dzadik <demeterdzadik@gmail.com>
2020-08-10 17:39:22 +0200

    finish testing D8519

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 rigify_info = {
 	'name': "CloudRig",
 	'author': "Demeter Dzadik",
+	'version' : (0, 0, 4),
 	'blender': (2, 82, 0),
 	'description': "Feature set developed by the Blender Animation Studio",
 	'doc_url': "https://gitlab.com/blender/CloudRig/-/wikis/",
@@ -19,9 +20,6 @@ import bpy, os
 from bpy.props import StringProperty
 from . import versioning
 
-def draw_preferences(layout, context, addon_prefs):
-	print("drawing CloudRig preferences...")
-	layout.label(text="Hello there.")
 
 # This allows you to right click on a button and link to documentation
 def cloudrig_manual_map():
