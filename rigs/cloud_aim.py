fe90a2df1c072b510e9708c3867cf9eb59b5898f
Mets3D <metssfm@gmail.com>
2020-10-28 21:06:14 +0100

    Fix error when aim rig is parentless

--- rigs/cloud_aim.py
+++ rigs/cloud_aim.py
@@ -145,7 +145,7 @@ class CloudAimRig(CloudBaseRig):
 			,head = aims_center
 			,tail = aims_center + group_vec.normalized() * self.scale/10
 			,bbone_width = 0.1
-			,parent = self.generator.find_bone_info(aim_bones[0].parent.name)
+			,parent = self.generator.find_bone_info(str(aim_bones[0].parent))
 		)
 
 		# Create the master bone.
