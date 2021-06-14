9f5cc127ac89bef7b4701edd9f6b670b16f16536
Demeter Dzadik <demeter@blender.studio>
2021-06-14 18:08:47 +0200

    Improve Mirror Rigify Parameters operator so it works with VectorProperties and EnumProperties

--- operators/mirror_rigify.py
+++ operators/mirror_rigify.py
@@ -4,25 +4,42 @@ from bpy.props import BoolProperty
 
 def copy_rigify_params(from_bone, to_bone, mirror=False):
 	for key, value in from_bone.rigify_parameters.items():
-		try:
-			if type(value)==str and mirror:
-				value = flip_name(value)
-			# Collection Properties!
-			elif type(value)==list:
-				other_coll = getattr(to_bone.rigify_parameters, key)
-				other_coll.clear()
-				for entry in value:
-					new = other_coll.add()
-					for sub_key in entry.to_dict().keys():
-						sub_value = entry.to_dict()[sub_key]
-						if type(sub_value)==str and mirror:
-							sub_value = flip_name(sub_value)
-						setattr(new, sub_key, sub_value)
-				continue
-			setattr(to_bone.rigify_parameters, key, value)
-		except:
-			pass
+		val_type = type(value)
 
+		if val_type == int and type(getattr(from_bone.rigify_parameters, key)) == str:
+			# NOTE: items() returns the integer value of enum properties, rather than the string value.
+			# So EnumProperty needs special treatment.
+			value_as_str = from_bone.rigify_parameters[key]
+			to_bone.rigify_parameters[key] = value_as_str
+			continue
+		elif val_type == list:
+			# If the rigify property is a CollectionProperty.
+			other_coll = getattr(to_bone.rigify_parameters, key)
+			other_coll.clear()
+			for entry in value:
+				new = other_coll.add()
+				for sub_key in entry.to_dict().keys():
+					sub_value = entry.to_dict()[sub_key]
+					if type(sub_value) == str and mirror:
+						sub_value = flip_name(sub_value)
+					setattr(new, sub_key, sub_value)
+			continue
+		elif val_type == IDPropertyArray:
+			# If the rigify property is any VectorProperty
+			if type(getattr(from_bone.rigify_parameters, key)[0]) == bool:
+				# If the rigify property is exactly BoolVectorProperty
+				setattr(to_bone.rigify_parameters, key, [bool(v) for v in value])
+			else:
+				setattr(to_bone.rigify_parameters, key, value[:])
+			continue
+		elif val_type == IDPropertyGroup:
+			# If the property is a dictionary, but as far as I know that is only possible for custom properties.
+			assert False, f"Mirroring of dictionary {key} not implemented. This should never happen?"
+
+		# Remaining cases: simple integers, floats, strings.
+		if val_type == str and mirror:
+			value = flip_name(value)
+		setattr(to_bone.rigify_parameters, key, value)
 class MirrorRigifyParameters(bpy.types.Operator):
 	"""Mirror rigify type and parameters of selected bones"""
 
