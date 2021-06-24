4b8bb58ef9b8ebbbc28504cceae51a795ea6d95f
Demeter Dzadik <demeter@blender.studio>
2021-06-24 16:20:34 +0200

    Change Regenerate Rigify Rigs to Generate Rigify Rigs in both name and behaviour

--- operators/regenerate_rigify_rigs.py
+++ operators/regenerate_rigify_rigs.py
@@ -4,11 +4,14 @@ from bpy.props import BoolProperty
 from ..utils.ui import is_cloud_metarig
 from ..utils.object import EnsureVisible
 
-def safe_generate(context, metarig, target_rig):
+def safe_generate(context, metarig):
 	# Generating requires the metarig to be the active object, and the target rig to be visible.
 
 	meta_visible = EnsureVisible(metarig)
-	rig_visible = EnsureVisible(target_rig)
+	target_rig = metarig.data.rigify_target_rig
+	rig_visible = None
+	if target_rig:
+		rig_visible = EnsureVisible(target_rig)
 
 	# Generate.
 	context.view_layer.objects.active = metarig
@@ -16,15 +19,28 @@ def safe_generate(context, metarig, target_rig):
 		bpy.ops.pose.cloudrig_generate()
 	else:
 		bpy.ops.pose.rigify_generate()
-		bpy.ops.object.cloudrig_refresh_drivers(selected_only=False)
+
+	bpy.ops.object.cloudrig_refresh_drivers(selected_only=False)
 
 	meta_visible.restore()
-	rig_visible.restore()
+	if rig_visible:
+		rig_visible.restore()
 
-class Regenerate_Rigify_Rigs(bpy.types.Operator):
-	""" Regenerate all Rigify rigs in the file. (Only works on metarigs that have an existing target rig.) """
-	bl_idname = "object.regenerate_all_rigify_rigs"
-	bl_label = "Regenerate All Rigify Rigs"
+def find_metarigs_in_scene(scene):
+	metarigs = []
+	for o in scene.objects:
+		if o.type != 'ARMATURE': continue
+		if 'rig_id' in o.data: continue
+		for pb in o.pose.bones:
+			if pb.rigify_type != "":
+				metarigs.append(o)
+				break
+	return metarigs
+
+class Generate_All_Rigify_Rigs(bpy.types.Operator):
+	"""Generate all Rigify rigs in the scene."""
+	bl_idname = "object.generate_all_rigify_rigs"
+	bl_label = "Generate All Rigify Rigs"
 	bl_options = {'REGISTER', 'UNDO'}
 
 	auto_hide: BoolProperty(
@@ -38,29 +54,25 @@ class Regenerate_Rigify_Rigs(bpy.types.Operator):
 		rigs_generated = 0
 		rigs_failed = 0
 		auto_mode = 'OBJECT'
-		for o in bpy.data.objects:
-			if o.type!='ARMATURE': continue
-			if o.data.rigify_target_rig:
-				metarig = o
-				if metarig==context.object:
-					auto_mode = metarig.mode
-				target_rig = o.data.rigify_target_rig
-				if target_rig:
-					try:
-						safe_generate(context, metarig, target_rig)
-						rigs_generated+=1
-					except:
-						rigs_failed += 1
-		if rigs_generated==0 and rigs_failed==0:
-			self.report({'INFO'}, "No rigs found to re-generate!")
-		elif rigs_generated>0 and rigs_failed >0:
+		for metarig in find_metarigs_in_scene(context.scene):
+			if metarig == context.object:
+				auto_mode = metarig.mode
+			try:
+				safe_generate(context, metarig)
+				rigs_generated += 1
+			except Exception as e:
+				rigs_failed += 1
+				raise e
+		if rigs_generated == 0 and rigs_failed == 0:
+			self.report({'INFO'}, "No rigs found to generate!")
+		elif rigs_generated > 0 and rigs_failed > 0:
 			self.report({'INFO'}, f"{rigs_failed} rig{'s' if rigs_failed>1 else ''} failed to generate. ({rigs_generated} succeeded.)")
-		elif rigs_failed>0:
+		elif rigs_failed > 0:
 			self.report({'ERROR'}, f"{rigs_failed} rig{'s' if rigs_failed>1 else ''} failed to generate. See the Rigify Log on the Metarig for more details.")
 		else:
 			self.report({'INFO'}, f"{rigs_generated} rig{'s' if rigs_generated>1 else ''} successfully generated!")
 
-		if self.auto_hide and rigs_generated==1 and rigs_failed==0:
+		if self.auto_hide and rigs_generated == 1 and rigs_failed == 0:
 			metarig.hide_set(True)
 			rig = metarig.data.rigify_target_rig
 			rig.hide_set(False)
@@ -73,8 +85,8 @@ class Regenerate_Rigify_Rigs(bpy.types.Operator):
 
 def register():
 	from bpy.utils import register_class
-	register_class(Regenerate_Rigify_Rigs)
+	register_class(Generate_All_Rigify_Rigs)
 
 def unregister():
 	from bpy.utils import unregister_class
-	unregister_class(Regenerate_Rigify_Rigs)
+	unregister_class(Generate_All_Rigify_Rigs)
