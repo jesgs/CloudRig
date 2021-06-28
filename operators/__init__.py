672cee1d6358eb8750834bf9e1168f331ad2cf77
Demeter Dzadik <demeter@blender.studio>
2021-06-28 17:09:40 +0200

    rename regenerate_rigify_rigs->generate_all_rigs

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,13 +1,11 @@
-import bpy
-
-from . import regenerate_rigify_rigs
+from . import generate_all_rigs
 from . import refresh_drivers
 from . import mirror_rigify
 from . import flatten_chain
 from . import toggle_metarig
 
 modules = [
-	regenerate_rigify_rigs,
+	generate_all_rigs,
 	refresh_drivers,
 	mirror_rigify,
     flatten_chain,
@@ -15,11 +13,9 @@ modules = [
 ]
 
 def register():
-	from bpy.utils import register_class
 	for m in modules:
 		m.register()
 
 def unregister():
-	from bpy.utils import unregister_class
 	for m in reversed(modules):
 		m.unregister()
