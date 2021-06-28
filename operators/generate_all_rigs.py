a054a99a48d8f872f6fca59f0f82dfd50a8181b5
Demeter Dzadik <demeter@blender.studio>
2021-06-28 17:11:46 +0200

    Generate All Rigs: Rework auto_hide param to be more powerful at restoring the context state

--- operators/generate_all_rigs.py
+++ operators/generate_all_rigs.py
@@ -43,45 +43,86 @@ class Generate_All_Rigify_Rigs(bpy.types.Operator):
 	bl_label = "Generate All Rigify Rigs"
 	bl_options = {'REGISTER', 'UNDO'}
 
-	auto_hide: BoolProperty(
-		name="Auto Hide/Unhide"
+	focus_generated: BoolProperty(
+		name="Focus Generated"
 		,default=False
-		,description="Enable additional convenience functionality when generating a single rig: After a successful generation, hide the metarig, unhide the generated rig, and enter the same mode on the generated rig as the current mode"
+		,description="After successfully generating a single rig, hide the metarig, unhide the generated rig, enter the same mode as the current mode, and match bone selection states where possible"
 	)
-	# TODO: Options to preserve pose and selected/active bones would be a nice bonus both here and in toggle_metarig.
 
 	def execute(self, context):
+		obj = context.object
 		rigs_generated = 0
 		rigs_failed = 0
-		auto_mode = 'OBJECT'
+
+		### Save state so it can be restored for convenience
+		state_mode = 'OBJECT'
+		state_active_bone = context.active_pose_bone.name if context.active_pose_bone else ""
+		state_selected_bones = [bone.name for bone in context.selected_pose_bones] if context.selected_pose_bones else []
+		state_hide_bones = {}
+		state_layers = []
+
+		### Attempt to generate all rigs, keep track of failures
 		for metarig in find_metarigs_in_scene(context.scene):
-			if metarig == context.object:
-				auto_mode = metarig.mode
+			if obj in [metarig, metarig.data.rigify_target_rig]:
+				state_mode = context.mode
+				if obj and metarig.data.rigify_target_rig == obj:
+					state_hide_bones = {bone.name : bone.hide for bone in obj.data.bones}
+					state_layers = obj.data.layers[:]
 			try:
 				safe_generate(context, metarig)
 				rigs_generated += 1
 			except Exception as e:
 				rigs_failed += 1
 				raise e
-		if rigs_generated == 0 and rigs_failed == 0:
-			self.report({'INFO'}, "No rigs found to generate!")
-		elif rigs_generated > 0 and rigs_failed > 0:
-			self.report({'INFO'}, f"{rigs_failed} rig{'s' if rigs_failed>1 else ''} failed to generate. ({rigs_generated} succeeded.)")
-		elif rigs_failed > 0:
-			self.report({'ERROR'}, f"{rigs_failed} rig{'s' if rigs_failed>1 else ''} failed to generate. See the Rigify Log on the Metarig for more details.")
-		else:
-			self.report({'INFO'}, f"{rigs_generated} rig{'s' if rigs_generated>1 else ''} successfully generated!")
+		
+		self.report_generation(rigs_generated, rigs_failed)
 
-		if self.auto_hide and rigs_generated == 1 and rigs_failed == 0:
-			metarig.hide_set(True)
-			rig = metarig.data.rigify_target_rig
-			rig.hide_set(False)
-			bpy.ops.object.mode_set(mode='OBJECT')
-			context.view_layer.objects.active = rig
-			rig.select_set(True)
-			bpy.ops.object.mode_set(mode=auto_mode)
+		if self.focus_generated and rigs_generated == 1 and rigs_failed == 0:
+			self.restore_state(context, metarig, state_mode, state_active_bone,
+						state_selected_bones, state_hide_bones, state_layers)
 
 		return { 'FINISHED' }
+
+	def report_generation(self, successes=0, failures=0):
+		"""Report whether metarigs generated successesfully or failed."""
+		if successes == 0 and failures == 0:
+			self.report({'INFO'}, "No rigs found to generate!")
+		elif successes > 0 and failures > 0:
+			self.report({'INFO'}, f"{failures} rig{'s' if failures>1 else ''} failed to generate. ({successes} succeeded.)")
+		elif failures > 0:
+			self.report({'ERROR'}, f"{failures} rig{'s' if failures>1 else ''} failed to generate. See the Rigify Log on the Metarig for more details.")
+		else:
+			self.report({'INFO'}, f"{successes} rig{'s' if successes>1 else ''} successfully generated!")
+
+	def restore_state(self, context, metarig, mode, active_bone_name="", selected_bone_names="", hide_bones={}, layers=[]):
+		"""Restore state for convenience."""
+		metarig.hide_set(True)
+		rig = metarig.data.rigify_target_rig
+		rig.hide_set(False)
+		bpy.ops.object.mode_set(mode='OBJECT')
+		context.view_layer.objects.active = rig
+		rig.select_set(True)
+
+		if mode in ['OBJECT', 'EDIT', 'POSE']:
+			bpy.ops.object.mode_set(mode=mode)
+		else:
+			print("HWAT THE FUCK")
+	
+		rig = context.object
+		if active_bone_name in rig.pose.bones:
+			rig.data.bones.active = rig.data.bones[active_bone_name]
+		
+		for bone_name in selected_bone_names:
+			if bone_name in rig.data.bones:
+				rig.data.bones[bone_name].select = True
+
+		if layers:
+			rig.data.layers = layers[:]
+
+		for bone_name in hide_bones.keys():
+			bone = rig.data.bones.get(bone_name)
+			if not bone: continue
+			bone.hide = hide_bones[bone_name]
 
 def register():
 	from bpy.utils import register_class
