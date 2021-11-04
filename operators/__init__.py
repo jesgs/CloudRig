62d19e9eba86fd8ad3f7c4f24e79f78c29ab195a
Demeter Dzadik <demeter@blender.studio>
2021-11-04 11:09:00 +0100

    Remove mirror_rigify since it's now in master.

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,10 +1,8 @@
-from . import mirror_rigify
 from . import flatten_chain
 from . import toggle_metarig
 from . import assign_bone_layers
 
 modules = [
-	mirror_rigify
 	,flatten_chain
 	,toggle_metarig
 	,assign_bone_layers
