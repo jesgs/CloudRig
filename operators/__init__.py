3f054a58f99c01f6cf47aa8aec13797a6337eeec
Demeter Dzadik <metemer001@gmail.com>
2022-02-17 18:35:36 +0100

    Add Edit Widget operator (pulled in from MetsTools)

--- operators/__init__.py
+++ operators/__init__.py
@@ -2,10 +2,12 @@ from . import flatten_chain
 from . import toggle_metarig
 from . import assign_bone_layers
 from . import toggle_action_constraints
+from . import edit_widget
 
 modules = [
 	flatten_chain
 	,toggle_metarig
 	,assign_bone_layers
 	,toggle_action_constraints
+	,edit_widget
 ]
