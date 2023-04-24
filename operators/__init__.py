f5d432cf30325e7aa12f31161be76778558ccc99
demeterdzadik@gmail.com <demeterdzadik@gmail.com>
2023-04-24 18:38:06 +0200

    New workflow operator: Select Bone By Name

--- operators/__init__.py
+++ operators/__init__.py
@@ -3,6 +3,7 @@ from . import toggle_metarig
 from . import assign_bone_layers
 from . import toggle_action_constraints
 from . import edit_widget
+from . import select_bone_by_name_relation
 
 modules = [
 	flatten_chain
@@ -10,4 +11,5 @@ modules = [
 	,assign_bone_layers
 	,toggle_action_constraints
 	,edit_widget
+    ,select_bone_by_name_relation
 ]
