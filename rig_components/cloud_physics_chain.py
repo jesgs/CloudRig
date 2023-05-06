b9d9b7f0bb2f6eb3423cf562124ff0362215c258
metemer001@gmail.com <metemer001@gmail.com>
2023-05-06 04:30:06 +0200

    Bring cloud_physics_chain out of _temp_ignore.

--- rig_components/cloud_physics_chain.py
+++ rig_components/cloud_physics_chain.py
@@ -1,5 +1,5 @@
 import bpy, bmesh
-
+from bpy.types import PropertyGroup
 from typing import List
 from ..rig_component_features.bone import BoneInfo
 
@@ -11,11 +11,11 @@ from .cloud_fk_chain import Component_Chain_FK
 
 class CloudPhysicsChainRig(Component_Chain_FK):
 	"""FK Chain with cloth physics."""
-
+	ui_name = "Chain: Physics"
 	forced_params = {
-		'CR_fk_chain_double_first' : False
-		,'CR_fk_chain_hinge' : False
-		,'CR_fk_chain_position_along_bone' : 0
+		'fk_chain.double_first' : False
+		,'fk_chain.hinge' : False
+		,'fk_chain.position_along_bone' : 0
 	}
 
 	def initialize(self):
@@ -24,8 +24,8 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 	def create_bone_infos(self):
 		super().create_bone_infos()
 
-		phys_ob = self.ensure_cloth_object(self.bone_sets['FK Controls'])
-		if self.params.CR_physics_chain_make_ctrl:
+		phys_ob = self.ensure_physics_object(self.bone_sets['FK Controls'])
+		if self.params.physics_chain.make_ctrl:
 			self.make_physics_chain(phys_ob, self.bone_sets['FK Controls'])
 		self.constrain_chain_to_phys_ob(phys_ob, self.bone_sets['FK Controls'])
 
@@ -43,25 +43,25 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 					self.obj.driver_remove(f'pose.bones["{org.name}"].constraints["{c.name}"].{d["prop"]}')
 				c.relink()
 
-	def ensure_cloth_object(self, bone_chain: List[BoneInfo]):
+	def ensure_physics_object(self, bone_chain: List[BoneInfo]):
 		context = bpy.context
 
-		cloth_ob = self.params.CR_physics_chain_object
-		if cloth_ob and not self.params.CR_physics_chain_force_regen:
-			return cloth_ob
+		phys_obj = self.params.physics_chain.phys_obj
+		if phys_obj and not self.params.physics_chain.force_regen:
+			return phys_obj
 
 		cloth_mesh = bpy.data.meshes.new(name=self.phys_name(self.base_bone) )
-		if not cloth_ob:
+		if not phys_obj:
 			# Create physics object.
-			cloth_ob = bpy.data.objects.new(cloth_mesh.name, cloth_mesh)
-			context.scene.collection.objects.link(cloth_ob)
-			cloth_ob.parent = self.obj
+			phys_obj = bpy.data.objects.new(cloth_mesh.name, cloth_mesh)
+			context.scene.collection.objects.link(phys_obj)
+			phys_obj.parent = self.obj
 		else:
-			cloth_ob.data = cloth_mesh
+			phys_obj.data = cloth_mesh
 
 		# Wipe modifiers & vertex groups
-		cloth_ob.modifiers.clear()
-		cloth_ob.vertex_groups.clear()
+		phys_obj.modifiers.clear()
+		phys_obj.vertex_groups.clear()
 
 		# Create verts and edges using bmesh.
 		bm = bmesh.new()
@@ -78,20 +78,19 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 		bm.to_mesh(cloth_mesh)
 		bm.free()
 
-
 		### Create and assign vertex groups.
 
 		# Total length of the chain
 		total_length = 0
 		for b in bone_chain:
 			total_length += b.length
-		total_length *= self.params.CR_physics_chain_pin_falloff_offset
+		total_length *= self.params.physics_chain.pin_falloff_offset
 		cum_length = 0
 
-		pin_name = "PIN-"+cloth_ob.name
+		pin_name = "PIN-"+phys_obj.name
 
 		# Assign weights.
-		pin_vg = cloth_ob.vertex_groups.new(name=pin_name)
+		pin_vg = phys_obj.vertex_groups.new(name=pin_name)
 		pin_vg.add([0], 1, 'REPLACE')
 		for i, v in enumerate(cloth_mesh.vertices):
 			if i==0: continue
@@ -99,27 +98,27 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 			name = self.phys_name(bone_chain[i-1])
 			# Determine pin weight on this vertex.
 			cum_length += bone_chain[i-1].length
-			ratio = self.params.CR_physics_chain_pin_falloff_offset - cum_length / total_length
-			if self.params.CR_physics_chain_pin_falloff == 'NONE':
+			ratio = self.params.physics_chain.pin_falloff_offset - cum_length / total_length
+			if self.params.physics_chain.pin_falloff == 'NONE':
 				pin_weight = 0
-			elif self.params.CR_physics_chain_pin_falloff == 'LINEAR':
+			elif self.params.physics_chain.pin_falloff == 'LINEAR':
 				pin_weight = ratio
-			elif self.params.CR_physics_chain_pin_falloff == 'QUADRATIC':
+			elif self.params.physics_chain.pin_falloff == 'QUADRATIC':
 				pin_weight = ratio*ratio
-			elif self.params.CR_physics_chain_pin_falloff == 'SQRT':
+			elif self.params.physics_chain.pin_falloff == 'SQRT':
 				pin_weight = sqrt(ratio)
 
-			vg = cloth_ob.vertex_groups.new(name=name)
+			vg = phys_obj.vertex_groups.new(name=name)
 			vg.add([i], 1, 'REPLACE')
 			pin_vg.add([i], pin_weight, 'REPLACE')
 
 		# Create Cloth modifier.
-		cloth_mod = cloth_ob.modifiers.new(type='CLOTH', name="Cloth")
+		cloth_mod = phys_obj.modifiers.new(type='CLOTH', name="Cloth")
 		cloth_mod.settings.vertex_group_mass = pin_name
 
 		bpy.ops.object.mode_set(mode='OBJECT')
-		context.view_layer.objects.active = cloth_ob
-		cloth_ob.select_set(True)
+		context.view_layer.objects.active = phys_obj
+		phys_obj.select_set(True)
 		bpy.ops.object.mode_set(mode='EDIT')
 		context.tool_settings.mesh_select_mode[0] = True
 		bpy.ops.mesh.select_all(action='SELECT')
@@ -129,9 +128,9 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 
 		context.view_layer.objects.active = self.obj
 		bpy.ops.object.mode_set(mode='EDIT')
-		self.params.CR_physics_chain_object = cloth_ob
-		self.meta_base_bone.rigify_parameters.CR_physics_chain_object = cloth_ob
-		return cloth_ob
+		self.params.physics_chain.phys_obj = phys_obj
+		self.meta_base_bone.rigify_parameters.CR_physics_chain_object = phys_obj
+		return phys_obj
 
 	def phys_name(self, thing):
 		return "PSX-" + self.naming.strip_org(thing)
@@ -151,7 +150,7 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 			next_parent = phys_ctrl
 
 		pin_bone = self.bone_sets['Physics Bones'].new(
-			name = "PIN-"+self.params.CR_physics_chain_object.name
+			name = "PIN-"+self.params.physics_chain.phys_obj.name
 			,source = self.bone_sets['Physics Bones'][0]
 			,parent = self.bone_sets['Physics Bones'][0]
 			,use_deform = True
@@ -180,26 +179,26 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 			)
 
 	def finalize(self):
-		cloth_ob = self.params.CR_physics_chain_object
+		phys_obj = self.params.physics_chain.phys_obj
 		context = bpy.context
 
-		if self.params.CR_physics_chain_make_ctrl:
+		if self.params.physics_chain.make_ctrl:
 			# Move armature modifier to top of the stack
-			context.view_layer.objects.active = cloth_ob
+			context.view_layer.objects.active = phys_obj
 			bpy.ops.object.modifier_move_to_index(modifier='Armature', index=0)
 			context.view_layer.objects.active = self.obj
-			cloth_ob.parent = None
+			phys_obj.parent = None
 		else:
-			# Parent cloth object.
-			cloth_ob.parent = self.obj
-			cloth_ob.parent_type = 'BONE'
+			# Parent physics object.
+			phys_obj.parent = self.obj
+			phys_obj.parent_type = 'BONE'
 			parent = self.bones_org[0].parent
 			if not parent:
 				parent = self.root_bone
-			cloth_ob.parent_bone = parent.name
+			phys_obj.parent_bone = parent.name
 
-			cloth_ob.matrix_parent_inverse = cloth_ob.matrix_world.inverted()
-			cloth_ob.matrix_world = Matrix.Identity((4))
+			phys_obj.matrix_parent_inverse = phys_obj.matrix_world.inverted()
+			phys_obj.matrix_world = Matrix.Identity((4))
 
 	##############################
 	# Parameters
@@ -210,46 +209,6 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 		cls.define_bone_set(params, 'Physics Bones', preset=3,	default_layers=[28])
 
 	@classmethod
-	def add_parameters(cls, params):
-		"""Add rig parameters to the RigifyParameters PropertyGroup."""
-		super().add_parameters(params)
-
-		params.CR_physics_chain_object = PointerProperty(
-			type		 = bpy.types.Object
-			,name		 = "Cloth Object"
-			,description = "Select an object which has vertex groups corresponding to the bone names of the chain, prefixed with 'phys_'. Leave empty to generate the object"
-		)
-		params.CR_physics_chain_force_regen = BoolProperty(
-			name		 = "Force Re-generate"
-			,description = "Even if the mesh already exists, force it to be re-generated from scratch"
-			,default	 = True
-		)
-		params.CR_physics_chain_pin_falloff = EnumProperty(
-			name		 = "Pin Falloff"
-			,description = "Type of falloff to apply to the generated cloth mesh's pin vertex group. The first vertex is always fully pinned"
-			,items		 = [
-				('NONE', "None", "First vertex fully pinned, rest fully unpinned"),
-				('LINEAR', "Linear", "First vertex fully pinned, last vertex not pinned at all, vertices inbetween are linear interpolated"),
-				('QUADRATIC', "Loose", "First vertex fully pinned, last vertex not pinned at all, vertices inbetween are linear interpolated and then raised to 2nd power"),
-				('SQRT', "Stiff", "First vertex fully pinned, last vertex not pinned at all, vertices inbetween are linear interpolated and then their square root is taken"),
-
-			]
-			,default	 = 'QUADRATIC'
-		)
-		params.CR_physics_chain_pin_falloff_offset = FloatProperty(
-			name		 = "Pin Falloff Offset"
-			,description = "Calculate the pin falloffs as if the bone chain was this much longer than it actually is. Increasing this beyond 1.0 will cause all vertices to be more pinned"
-			,default	 = 1.20
-			,min		 = 0.0
-			,max		 = 10.0
-		)
-		params.CR_physics_chain_make_ctrl = BoolProperty(
-			name		 = "Create Physics Controls"
-			,description = "Create a control chain that can control the physics mesh using an Armature modifier"
-			,default	 = True
-		)
-
-	@classmethod
 	def draw_control_params(cls, layout, context, params):
 		"""Create the ui for the rig parameters."""
 		super().draw_control_params(layout, context, params)
@@ -257,15 +216,52 @@ class CloudPhysicsChainRig(Component_Chain_FK):
 		layout.separator()
 		cls.draw_control_label(layout, "Physics")
 
-		cls.draw_prop(layout, params, 'CR_physics_chain_object')
-		cls.draw_prop(layout, params, 'CR_physics_chain_force_regen')
+		cls.draw_prop(layout, params.physics_chain, 'phys_obj')
+		cls.draw_prop(layout, params.physics_chain, 'force_regen')
 
-		if not params.CR_physics_chain_object or params.CR_physics_chain_force_regen:
-			cls.draw_prop(layout, params, 'CR_physics_chain_pin_falloff')
-			if params.CR_physics_chain_pin_falloff != 'NONE':
-				cls.draw_prop(layout, params, 'CR_physics_chain_pin_falloff_offset')
+		if not params.physics_chain.phys_obj or params.physics_chain.force_regen:
+			cls.draw_prop(layout, params.physics_chain, 'pin_falloff')
+			if params.physics_chain.pin_falloff != 'NONE':
+				cls.draw_prop(layout, params.physics_chain, 'pin_falloff_offset')
 
-		cls.draw_prop(layout, params, 'CR_physics_chain_make_ctrl')
+		cls.draw_prop(layout, params.physics_chain, 'make_ctrl')
+
+
+class Params(PropertyGroup):
+	phys_obj: PointerProperty(
+		type		 = bpy.types.Object
+		,name		 = "Cloth Object"
+		,description = "Select an object which has vertex groups corresponding to the bone names of the chain, prefixed with 'phys_'. Leave empty to generate the object"
+	)
+	force_regen: BoolProperty(
+		name		 = "Force Re-generate"
+		,description = "Even if the mesh already exists, force it to be re-generated from scratch"
+		,default	 = True
+	)
+	pin_falloff: EnumProperty(
+		name		 = "Pin Falloff"
+		,description = "Type of falloff to apply to the generated cloth mesh's pin vertex group. The first vertex is always fully pinned"
+		,items		 = [
+			('NONE', "None", "First vertex fully pinned, rest fully unpinned"),
+			('LINEAR', "Linear", "First vertex fully pinned, last vertex not pinned at all, vertices inbetween are linear interpolated"),
+			('QUADRATIC', "Loose", "First vertex fully pinned, last vertex not pinned at all, vertices inbetween are linear interpolated and then raised to 2nd power"),
+			('SQRT', "Stiff", "First vertex fully pinned, last vertex not pinned at all, vertices inbetween are linear interpolated and then their square root is taken"),
+
+		]
+		,default	 = 'QUADRATIC'
+	)
+	pin_falloff_offset: FloatProperty(
+		name		 = "Pin Falloff Offset"
+		,description = "Calculate the pin falloffs as if the bone chain was this much longer than it actually is. Increasing this beyond 1.0 will cause all vertices to be more pinned"
+		,default	 = 1.20
+		,min		 = 0.0
+		,max		 = 10.0
+	)
+	make_ctrl: BoolProperty(
+		name		 = "Create Physics Controls"
+		,description = "Create a control chain that can control the physics mesh using an Armature modifier"
+		,default	 = True
+	)
 
 class RigComponent(CloudPhysicsChainRig):
 	pass
