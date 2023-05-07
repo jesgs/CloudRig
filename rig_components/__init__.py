6395228464764be9f3452c09eeb3be2f00bbb789
metemer001@gmail.com <metemer001@gmail.com>
2023-05-07 02:00:20 +0200

    Slightly simplify rig component loading

--- rig_components/__init__.py
+++ rig_components/__init__.py
@@ -1,42 +1,32 @@
 import os, importlib
 from typing import Dict
 from pathlib import Path
+import bpy
 
 rig_modules = {}
 
 def load_component_modules(dir_path: str, package: str) -> Dict:
-    """This function is not only important to populate the global variable rig_modules defined above.
-
-    It also manualy imports the rig modules, which is necessary because 
-    >>>Blender only recognizes modules that are imported BY NAME.<<<
-
-    For example, if we were to simply put `from . import *` in this file, and then
-    tried accessing the `CloudRig.rig_components.cloud_aim` module from Blender's PyConsole, 
-    it WILL NOT WORK.
-
-    If we were to put `from . import cloud_aim`, then it WILL WORK in the PyConsole.
-    But we don't want to do that, because I want to be able to add rig implementation
-    files without having to modify any surrounding code.
+    """Manualy imports the rig modules, since they don't get automatically
+    loaded because they aren't referenced by the code directly.
     """
+
     files = os.listdir(dir_path)
+    module_info = bpy.path.module_names(dir_path)
 
     components = {}
 
-    for f in files:
-        path = Path(os.path.join(dir_path, f))
-        is_dir = os.path.isdir(path.as_posix())
-
-        if f[0] in {'.', '_'}:
+    for module_name, module_filepath in module_info:
+        folder_path = os.path.dirname(__file__)
+        # This terrbileness is needed because import_module() does not work
+        # with absolute paths containing a period (which Blender scripts 
+        # always do because of the version number folder).
+        delta = module_filepath.replace(folder_path, "").replace("\\", ".").replace(".py", "")
+        if module_name.startswith("_") or module_filepath.endswith("__init__"):
             continue
-        
-        if is_dir:
-            sub_components = load_component_modules(path.as_posix(), package=package+"."+f)
-            components.update(sub_components)
-        elif f.endswith(".py"):
-            filename = f[:-3]
-            rig_module = importlib.import_module("."+filename, package=package)
-            if hasattr(rig_module, 'RigComponent'):
-                components[filename] = rig_module
+        module = importlib.import_module(delta, __package__)
+        if not hasattr(module, 'RigComponent'):
+            continue
+        components[module_name] = module
 
     return components
 
