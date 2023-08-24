62b823e99ecf8388c5fa5f3e2050ffaaa1fc0d26
demeterdzadik@gmail.com <demeterdzadik@gmail.com>
2023-08-24 15:48:43 +0200

    Fix failing to register on Linux

--- rig_components/__init__.py
+++ rig_components/__init__.py
@@ -9,18 +9,13 @@ def load_component_modules(dir_path: str, package: str) -> Dict:
     """Manualy imports the rig modules, since they don't get automatically
     loaded because they aren't referenced by the code directly.
     """
-
-    files = os.listdir(dir_path)
     module_info = bpy.path.module_names(dir_path)
-
     components = {}
-
     for module_name, module_filepath in module_info:
-        folder_path = os.path.dirname(__file__)
         # This terrbileness is needed because import_module() does not work
         # with absolute paths containing a period (which Blender scripts 
         # always do because of the version number folder).
-        delta = module_filepath.replace(folder_path, "").replace("\\", ".").replace(".py", "")
+        delta = module_filepath.replace(dir_path, "").replace(os.sep, ".").replace(".py", "")
         if module_name.startswith("_") or module_filepath.endswith("__init__"):
             continue
         module = importlib.import_module(delta, __package__)
