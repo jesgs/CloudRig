3d7617ceb91ffc8491b54ceddc8f7d464f4a736c
demeterdzadik@gmail.com <demeterdzadik@gmail.com>
2024-04-18 16:13:27 +0200

    Fix false-positive warning log of invalid component types

--- generation/cloud_generator.py
+++ generation/cloud_generator.py
@@ -414,7 +414,7 @@ class CloudRig_Generator(TestAnimationGeneratorMixin):
             for pb in sorted(
                 self.metarig.pose.bones, key=lambda pb: pb.cloudrig_component.order
             )
-            if pb.cloudrig_component.enabled_with_parents
+            if pb.cloudrig_component.component_type and pb.cloudrig_component.enabled_with_parents
         ]
 
         comp_map = OrderedDict()
