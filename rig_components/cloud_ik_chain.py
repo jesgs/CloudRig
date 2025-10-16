jesgs/CloudRig

commit 209d2cda3393a5f4736e6b975926df33dbc97d68

Cleanup(?): De-duplicate code w/ new create_driven_armature_constraint() func
Didn't test anything.

rig_components/cloud_ik_chain.py
@@ -613,38 +613,9 @@ class Component_Chain_IKFK(Component_Chain_FK):
         else:
             first_parent = self.bones_org[0].parent
 
-        arm_con = parent_helper.add_constraint(
-            'ARMATURE',
-            use_deform_preserve_volume=True,
-            targets=[
-                {'subtarget': first_parent},
-                {
-                    'subtarget': ik_mstr
-                },  # TODO: This could be stretch_bone but that breaks Sprite Fright animations and doesn't work well in some cases.
-            ],
-        )
 
         ik_pole_follow_name = "ik_pole_follow_" + self.limb_name_props
-        # Add driver to the new constraint target.
-        driver1 = {
-            'prop': 'targets[0].weight',
-            'expression': '1-follow',
-            'variables': {
-                'follow': {
-                    'type': 'SINGLE_PROP',
-                    'targets': [
-                        {
-                            'data_path': f'pose.bones["{self.properties_bone.name}"]["{ik_pole_follow_name}"]'
-                        }
-                    ],
-                }
-            },
-        }
-        driver2 = dict(driver1)
-        driver2['prop'] = 'targets[1].weight'
-        driver2['expression'] = 'follow'
-        arm_con.drivers.append(driver1)
-        arm_con.drivers.append(driver2)
+        self.create_driven_armature_constraint(parent_helper, target_bones=[ik_mstr, first_parent], prop_bone=self.properties_bone, prop_name=ik_pole_follow_name, preserve_volume=True)
 
         # Let Stretch Helper copy rotation of IK master, for nice controlling of the IK Pole.
         stretch_bone.add_constraint('COPY_ROTATION', index=0, subtarget=ik_mstr)

rig_component_features/parenting.py
@@ -3,6 +3,7 @@
 from bpy.types import PropertyGroup, UIList
 from bpy.props import StringProperty, CollectionProperty, IntProperty, BoolProperty
 from bl_ui.generic_ui_list import draw_ui_list
+from .bone_info import BoneInfo
 
 from ..utils.rig import get_pbone_of_active
 from .component_params_ui import draw_label_with_linebreak
@@ -122,16 +123,9 @@ class CloudParentingMixin:
         row_name = row_name or child_bone.name.split(".")[0]
         entry_name = entry_name or child_bone.name
 
-        # Create parent bone that will hold the Armature constraint.
-        arm_con_bone = self.create_parent_bone(child_bone, self.bones_mch)
-        arm_con_bone.name = "P-" + child_bone.name
-        arm_con_bone.custom_shape = None
-
         parent_ui_names, parent_bone_names = self.sanitize_parent_list(parent_slots)
         if not parent_ui_names:
             return
-
-        targets = [{'subtarget': bone_name} for bone_name in parent_bone_names]
 
         # Create custom property
         if len(parent_bone_names) > 1:
@@ -162,10 +156,18 @@ class CloudParentingMixin:
                 },
             )
 
-        # Add armature constraint
-        arm_con = arm_con_bone.add_constraint('ARMATURE', name="Armature (Parent Switching)", targets=targets)
+        # Create parent bone that will hold the Armature constraint.
+        arm_con_bone = self.create_parent_bone(child_bone, self.bones_mch)
+        arm_con_bone.custom_shape = None
+        self.create_driven_armature_constraint(arm_con_bone, parent_bone_names, prop_bone.name, prop_name)
 
-        if len(parent_bone_names) == 1:
+    def create_driven_armature_constraint(self, bone: BoneInfo, target_bones: list[BoneInfo|str], prop_bone: BoneInfo|str, prop_name: str, preserve_volume=False):
+        targets = [{'subtarget': bone} for bone in target_bones]
+
+        # Add armature constraint
+        arm_con = bone.add_constraint('ARMATURE', name="Armature (Parent Switching)", targets=targets, use_deform_preserve_volume=preserve_volume)
+
+        if len(target_bones) == 1:
             return
 
         # Add weight drivers
@@ -179,7 +181,7 @@ class CloudParentingMixin:
                             'type': 'SINGLE_PROP',
                             'targets': [
                                 {
-                                    'data_path': f'pose.bones["{prop_bone.name}"]["{prop_name}"]'
+                                    'data_path': f'pose.bones["{prop_bone}"]["{prop_name}"]'
                                 }
                             ],
                         }

rig_components/cloud_aim.py
@@ -184,19 +184,7 @@ class Component_Aim(Component_Base):
                 },
             )
 
-            arm_con = highlight_ctr.add_constraint(
-                'ARMATURE',
-                targets=[{'subtarget': ctr_bone.name}, {'subtarget': ctr_bone.parent.name}],
-            )
-            driver = {
-                'prop': 'targets[0].weight',
-                'variables': [(highlight_ctr.name, prop_name)],
-            }
-            arm_con.drivers.append(driver)
-            driver = dict(driver)
-            driver['prop'] = 'targets[1].weight'
-            driver['expression'] = '1-var'
-            arm_con.drivers.append(driver)
+            self.create_driven_armature_constraint(highlight_ctr, target_bones=[ctr_bone, ctr_bone.parent], prop_bone=highlight_ctr, prop_name=prop_name)
 
         self.lock_transforms(highlight_ctr, loc=False, rot=False, scale=[False, True, False])
 

rig_components/cloud_fk_chain.py
@@ -202,24 +202,7 @@ class Component_Chain_FK(Component_ToonChain, CloudAnimationMixin):
                 rotation_mode=fk_bone.rotation_mode,
             )
             next_parent = reverse_fk
-            arm_con = fk_bone.add_constraint('ARMATURE', targets=[{'subtarget': fk_bone.parent.name}, {'subtarget': reverse_fk.name}])
-            drv1 = {
-                'prop': 'targets[0].weight',
-                'expression': '1-var',
-                'variables': {
-                    'var': {
-                        'type': 'SINGLE_PROP',
-                        'targets': [
-                            {
-                                'data_path': f'pose.bones["{self.properties_bone.name}"]["{self.reverse_fk_name}"]'
-                            }
-                        ],
-                    }
-                }
-            }
-            drv2 = drv1.copy()
-            drv2.update({'expression': 'var', 'prop': 'targets[1].weight'})
-            arm_con.drivers.extend([drv1, drv2])
+            self.create_driven_armature_constraint(fk_bone, target_bones=[fk_bone.parent, reverse_fk], prop_bone=self.properties_bone, prop_name=self.reverse_fk_name)
 
         self.add_bone_property_with_ui(
             prop_bone=self.properties_bone,
@@ -362,23 +345,7 @@ class Component_Chain_FK(Component_ToonChain, CloudAnimationMixin):
             },
         )
 
-        # Hinge Armature constraint
-        hng_con = hng_bone.add_constraint(
-            'ARMATURE',
-            targets=[{"subtarget": root_bone}, {"subtarget": str(parent_bone)}],
-        )
-
-        hng_con.drivers.append(
-            {'prop': 'targets[0].weight', 'variables': [(prop_bone.name, prop_name)]}
-        )
-
-        hng_con.drivers.append(
-            {
-                'prop': 'targets[1].weight',
-                'expression': '1-var',
-                'variables': [(prop_bone.name, prop_name)],
-            }
-        )
+        self.create_driven_armature_constraint(hng_bone, target_bones=[parent_bone, root_bone], prop_bone=prop_bone, prop_name=prop_name)
 
         # Hinge Copy Location & Scale constraints
         hng_bone.add_constraint(

rig_components/cloud_jaw.py
@@ -66,7 +66,7 @@ class Component_Jaw(Component_CopyBone):
                 Copy Transforms (Local): Jaw
     """
 
-    ui_name = 'Jaw'
+    ui_name = 'Jaw (Legacy)'
 
     def create_bone_infos(self, context):
         super().create_bone_infos(context)

rig_components/cloud_leg.py
@@ -388,24 +388,12 @@ class Component_Limb_BipedLeg(Component_Limb):
         # FK Toe bone should be parented between FK Foot and IK Toe.
         fk_toe = self.fk_toe
         fk_toe.parent = None
-        toe_con = fk_toe.add_constraint(
-            'ARMATURE',
-            targets=[
-                {"subtarget": self.bones_org[-2].fk_bone.name},  # FK Foot
-                {"subtarget": self.ik_chain[-1].name},  # IK Toe
-            ],
+        self.create_driven_armature_constraint(
+            fk_toe,
+            target_bones=[self.ik_chain[-1], self.bones_org[-2].fk_bone],
+            prop_bone=self.properties_bone,
+            prop_name=self.ikfk_name
         )
-
-        ik_driver = {
-            'prop': 'targets[1].weight',
-            'variables': [(self.properties_bone.name, self.ikfk_name)],
-        }
-        toe_con.drivers.append(ik_driver)
-
-        fk_driver = deepcopy(ik_driver)
-        fk_driver['expression'] = "1-var"
-        fk_driver['prop'] = 'targets[0].weight'
-        toe_con.drivers.append(fk_driver)
 
     def tweak_org_foot(self):
         # Delete IK constraint and driver from toe bone. It should always use FK.

rig_components/cloud_spine_squashy.py
@@ -28,7 +28,7 @@ class Component_Spine_Squashy(Component_Chain_FK):
                 "Spine rig must consist of a chain of at least 2 connected bones!"
             )
 
-        self.spine_name = self.naming.slice_name(self.base_bone_name)[1]
+        self.spine_name = self.params.base.base_name or self.naming.slice_name(self.base_bone_name)[1]
         self.squashy_name = "squashy_spine_" + self.spine_name.lower()
         self.squashy_volume_name = "squashy_spine_volume_" + self.spine_name.lower()
 

commit 492aaa79754a9437435dec7a6b4d892cc0cae7a7

Fix IK pole follow broken by 209d2cda3
Also make parenting transition gradual by making the driver expression a bit smarter

rig_components/cloud_ik_chain.py
@@ -615,7 +615,8 @@ class Component_Chain_IKFK(Component_Chain_FK):
 
 
         ik_pole_follow_name = "ik_pole_follow_" + self.limb_name_props
-        self.create_driven_armature_constraint(parent_helper, target_bones=[ik_mstr, first_parent], prop_bone=self.properties_bone, prop_name=ik_pole_follow_name, preserve_volume=True)
+            pole_parent_helper,
+            target_bones=[first_parent, ik_mstr],
 
         # Let Stretch Helper copy rotation of IK master, for nice controlling of the IK Pole.
         stretch_bone.add_constraint('COPY_ROTATION', index=0, subtarget=ik_mstr)