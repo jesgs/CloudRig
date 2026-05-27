841d9388b82b127abdd9ebecf66f4d197c44daa1
Demeter Dzadik <demeterdzadik@gmail.com>
2026-05-27 14:52:06 +0200

    Finger: Fix default_fkik value not working

--- CloudRig/rig_components/cloud_finger.py
+++ CloudRig/rig_components/cloud_finger.py
@@ -40,7 +40,6 @@ class Component_Finger(Component_Chain_IKFK):
         # TODO: This should be restructured, we shouldn't be overriding this function.
         if panel_name == "FK/IK Switch":
             label_name = n_("FK/IK Switch")
-            custom_prop_settings['default'] = 0.0
 
         panel_name = n_("Fingers")
         if label_name == "IK Pole Follow":
@@ -94,24 +93,24 @@ class Component_Finger(Component_Chain_IKFK):
         """Avoid creating complex inherited setup. Just parent the pole to the master."""
         ik_pole.parent = ik_mstr
 
-    @no_overlay(return_value={})
-    def ik_chain__get_ik_switch_ui_data(self, fk_chain, ik_chain, ik_mstr, ik_pole) -> dict:
-        ui_data = super().ik_chain__get_ik_switch_ui_data(fk_chain, ik_chain, ik_mstr, ik_pole)
+    # @no_overlay(return_value={})
+    # def ik_chain__get_ik_switch_ui_data(self, fk_chain, ik_chain, ik_mstr, ik_pole) -> dict:
+    #     ui_data = super().ik_chain__get_ik_switch_ui_data(fk_chain, ik_chain, ik_mstr, ik_pole)
 
-        # It's quite strange to be creating an extra helper bone in this function,
-        # but we need it for correct snapping in this case.
-        tip_str = self.main_str_bones[-1]
-        snap_helper = self.bone_sets['Mechanism Bones'].new(
-            source=tip_str,
-            parent=tip_str,
-            name=self.naming.add_prefix(ik_mstr, "SNAP"),
-            use_inherit_rotation=False,
-        )
+    #     # It's quite strange to be creating an extra helper bone in this function,
+    #     # but we need it for correct snapping in this case.
+    #     tip_str = self.main_str_bones[-1]
+    #     snap_helper = self.bone_sets['Mechanism Bones'].new(
+    #         source=tip_str,
+    #         parent=tip_str,
+    #         name=self.naming.add_prefix(ik_mstr, "SNAP"),
+    #         use_inherit_rotation=False,
+    #     )
 
-        map_on = [(ik_mstr.name, snap_helper.name)]
+    #     map_on = [(ik_mstr.name, snap_helper.name)]
 
-        ui_data['op_kwargs']['map_on'] = map_on
-        return ui_data
+    #     ui_data['op_kwargs']['map_on'] = map_on
+    #     return ui_data
 
     @no_overlay
     def ik_chain__attach_org_to_ik(self, org_chain: list[BoneInfo], ik_chain: list[BoneInfo]):
